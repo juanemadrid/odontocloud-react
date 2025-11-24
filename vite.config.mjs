70ede36a803e7249a2e03e1ed3322db4160065a9
Juan Eduardo Madrid <madridsystem@outlook.es>
2025-11-24 16:35:13 -0500

    1

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,10 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// En desarrollo: base = '/'  (no hay subcarpeta)
+// En producción (build/deploy): base = '/odontocloud-react/'
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === 'development' ? '/' : '/odontocloud-react/',
+}))
