6ccfdcb4cf2b81e6654c1a2b52816eef72ede199
Juan Eduardo Madrid <madridsystem@outlook.es>
2025-11-25 10:07:13 -0500

    vd

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración para GitHub Pages
+// ⚙️ Configuración para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/odontocloud-react/',  // 👈 MUY IMPORTANTE: nombre exacto del repo
+  base: '/odontocloud-react/', // 👈 nombre exacto del repositorio
   build: {
-    outDir: 'dist'              // Carpeta donde se guarda el build
+    outDir: 'dist' // carpeta donde se guarda el build
   }
 })
